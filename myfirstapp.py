973e6429ff69e7d0fd072d31693a4aa3176c61a1
wiepeng <wiepeng@gmail.com>
2024-11-13 15:49:52 +0800

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -19,10 +19,11 @@ elif option=='map':
     columns=['lat', 'lon'])
     st.map(map_data)
 
-st.write('Before you continue, please read the [terms and conditions](https://www.gnu.org/licenses/gpl-3.0.en.html)')
-show = st.checkbox('I agree the terms and conditions')
-if show:
-    st.write(pd.DataFrame({
-    'Students': ['John', 'Lofa', 'Siti', 'Amy'],
-    'Attendance Status': ['yes', 'yes', 'yes', 'no']
-    }))
+elif option=='T n C':
+    st.write('Before you continue, please read the [terms and conditions](https://www.gnu.org/licenses/gpl-3.0.en.html)')
+    show = st.checkbox('I agree the terms and conditions')
+    if show:
+        st.write(pd.DataFrame({
+          'Students': ['John', 'Lofa', 'Siti', 'Amy'],
+          'Attendance Status': ['yes', 'yes', 'yes', 'no']
+        }))

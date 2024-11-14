77c829a4586160985e83dc1f6ce43be520c9de72
wiepeng <wiepeng@gmail.com>
2024-11-14 14:18:20 +0800

    Create sale_prediction.py

---
+++ sale_prediction.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+import pickle
+import sklearn
+import sklearn import preprocessing
+
+loaded_model = pickle.load(open "sale-advertising-model.h5","rb")
+
+                        
+
+
+st.header("My first Streamlit App")
+
+st.write('Before you continue, please read the [terms and conditions](https://www.gnu.org/licenses/gpl-3.0.en.html)')
+show = st.checkbox('I agree the terms and conditions')
+if show:
+    st.write(pd.DataFrame({
+    'Students': ['John', 'Lofa', 'Siti', 'Amy'],
+    'Attendance Status': ['yes', 'yes', 'yes', 'no']
+    }))
